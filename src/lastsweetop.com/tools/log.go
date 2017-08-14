ef3d8ca93fc06ccad950a07b91e4f1f129b20ec1
lastsweetop <sweetop@189.cn>
2017-08-14 20:17:41 +0800

    test log
    Body in detail

---
+++ src/lastsweetop.com/tools/log.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func log(message string) {
+	fmt.Println(message)
+}
--- src/lastsweetop.com/tools/main.go
+++ src/lastsweetop.com/tools/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello Goone")
+	log("this is log test")
 }
